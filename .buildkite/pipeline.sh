4ae1f420e11b93b8461c44ce69d8eb2af0d56d8c
Andriy Yanko <andriy.yanko@gmail.com>
2020-10-22 19:48:02 +0300

    Update pipeline

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -9,14 +9,21 @@ GH_API_BRANCH="${BUILDKITE_BRANCH}"
 echo "steps:"
 
 for gem in gems/*; do
-  count=$(curl -s \
+  master_sha=$(curl -s \
+    -H "Authorization: token ${GH_TOKEN}" \
+    -X GET \
+    "https://api.github.com/repos/${GH_API_REPO}/commits?sha=master&path=${gem}&per_page=1" \
+    | jq '.[0].sha'
+  )
+
+  current_sha=$(curl -s \
     -H "Authorization: token ${GH_TOKEN}" \
     -X GET \
     "https://api.github.com/repos/${GH_API_REPO}/commits?sha=${GH_API_BRANCH}&path=${gem}&per_page=1" \
-    | jq '. | length'
+    | jq '.[0].sha'
   )
 
-  if [[ $count == "1" ]]; then
+  if [[ "${current_sha}" != "${master_sha}" ]]; then
     echo "  - name: \":rspec:\""
     echo "    command: \"cd ${gem} && bundle install && rake spec\""
     echo "    timeout_in_minutes: 10"
