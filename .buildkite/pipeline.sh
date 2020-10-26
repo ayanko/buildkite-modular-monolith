bdb187dcdedee1ebe7d20356ebd08f4233ec5ecc
Andriy Yanko <andriy.yanko@gmail.com>
2020-10-26 18:42:52 +0200

    Switch to better implementation

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -2,45 +2,98 @@
 
 set -eu
 
-GH_API_TOKEN="${GH_TOKEN}"
-GH_API_REPO="ayanko/buildkite-modular-monolith"
-GH_API_BRANCH="${BUILDKITE_BRANCH}"
+###############################################################################
 
-changed_gems=()
+readonly MODULAR_ROOTS=(gems)
+readonly GH_REPO_URL_REGEXP='^git\@github\.com\:(.*)\.git$'
 
-for gem in gems/*; do
-  master_sha=$(curl -s \
-    -H "Authorization: token ${GH_TOKEN}" \
-    -X GET \
-    "https://api.github.com/repos/${GH_API_REPO}/commits?sha=master&path=${gem}&per_page=1" \
-    | jq '.[0].sha'
-  )
+###############################################################################
 
-  current_sha=$(curl -s \
-    -H "Authorization: token ${GH_TOKEN}" \
-    -X GET \
-    "https://api.github.com/repos/${GH_API_REPO}/commits?sha=${GH_API_BRANCH}&path=${gem}&per_page=1" \
-    | jq '.[0].sha'
-  )
+function error_exit {
+  echo "${1:-"Unknown Error"}" >&2
+  exit 1
+}
 
-  if [[ "${current_sha}" != "${master_sha}" ]]; then
-    changed_gems+=(${gem})
+function get_gh_api_base {
+  if [[ ${BUILDKITE_REPO} =~ ${GH_REPO_URL_REGEXP} ]]; then
+    echo "https://api.github.com/repos/${BASH_REMATCH[1]}"
+  else
+    error_exit "Can't parse BUILDKITE_REPO=${BUILDKITE_REPO}"
   fi
-done
+}
 
-if [[ ${#changed_gems[@]} -eq 0 ]]; then
-  echo "steps: []"
-else
-  echo "steps:"
-  for gem in ${changed_gems[@]}; do
-    echo "  - name: \":rspec:\""
-    echo "    command: \"cd ${gem} && bundle install && rake spec\""
-    echo "    timeout_in_minutes: 10"
-    echo "    retry:"
-    echo "      automatic: true"
-    echo "    plugins:"
-    echo "      docker-compose#v3.3.0:"
-    echo "        run: test"
-    echo "        pull-retries: 4"
-  done
+function gh_api_request {
+  local url="${GH_API_BASE}/$1"
+  local output=$(
+    curl -s \
+    -H "Authorization: token ${GH_TOKEN}" \
+    -w "%{http_code}" \
+    -X GET "$url"
+  )
+
+  local code=$(
+    echo "$output" | tail -n1
+  )
+
+  local body=$(
+    echo "$output" | sed '$ d'
+  )
+
+  if [[ $code != 200 ]]; then
+    error_exit "GH API ERROR: url=$url code=$code body=$body"
+  fi
+
+  echo "$body"
+}
+
+function get_content {
+  local path="$1"
+  local branch="$2"
+  gh_api_request "contents/$path?ref=$branch"
+}
+
+function get_dir_sha {
+  local content="$1"
+  local dir="$2"
+  echo "$content" | jq -r ".[] | select(.type==\"dir\" and .path==\"$dir\") | .sha"
+}
+
+###############################################################################
+
+readonly GH_API_BASE=$(get_gh_api_base)
+
+enforce_all=false
+if [[ "$BUILDKITE_BRANCH" == "master" ]]; then
+  enforce_all=true
 fi
+readonly enforce_all
+
+cat .buildkite/pipeline.yml
+
+for root in ${MODULAR_ROOTS[@]}; do
+  if [[ $enforce_all == false ]]; then
+    master_content=$(get_content "$root" "master")
+    branch_content=$(get_content "$root" "$BUILDKITE_BRANCH")
+  fi
+
+  for path in $root/*; do
+    if [[ $enforce_all == true ]]; then
+      dir_changed=true
+    else
+      master_dir_sha=$(get_dir_sha "$master_content" "$path")
+      branch_dir_sha=$(get_dir_sha "$branch_content" "$path")
+      if [[ "$master_dir_sha" != "$branch_dir_sha" ]]; then
+        dir_changed=true
+      else
+        dir_changed=false
+      fi
+    fi
+
+    if [[ $dir_changed == true ]]; then
+      cat "$path/.buildkite/_pipeline.yml"
+    else
+      echo "  - label: \":point_up:\""
+      echo "    command: echo \"Skip ${path}\""
+    fi
+  done
+done
